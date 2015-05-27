c91817d268a99303249525baf6fddf57b20e0331
Syed Nazrul <ssnazrul@starlight.sdsc.edu>
2015-05-27 16:59:58 -0700

    update analysis with regex and part of csv

--- analyze.py
+++ analyze.py
@@ -1,16 +1,19 @@
-_author_='Syed Sadat Nazrul'
-
 import re
+import numpy as np
+import csv
 
-filename = 'cat.out'
-file=open('cat.out')
-str2search=''
-for message in file:
-	str2search += message
-word_search=re.search('(?!1\s+)([\d\.]+)',str2search)
-result=word_search.group()
-print result
-
+def combinations(array):
+    '''
+    Generates every combination of an array
+    :param array (list): List of nodes
+    :return: combinations (list)
+    '''
+    combo=[]
+    for i in array:
+        for j in array:
+            if i != j and set([i,j]) not in combo:
+                combo.append([i,j])
+    return combo
 
 def analyze(filenames):
     fieldnames=['#']
@@ -24,6 +27,39 @@ def analyze(filenames):
         writer.writerow(r)
     print("Results written to results.csv!")
 
+
+
+filename='comet-17-[12-14].out'
+file=open(filename)
+str2search=''
+latency_times=[]
+for message in file:
+	str2search += message
+file.close()
+word_search=re.findall('(1\s+)([\d\.]+)',str2search)
+for sub_group in word_search:
+    if '1\n' not in sub_group:
+        latency_times.append(float(sub_group[1]))
+
+rack=int(filename[6:8])
+node1=int(filename[10:12])
+node2=int(filename[13:15])
+array=combinations(np.linspace(node1,node2, node2-node1+1))
+fieldnames=['#']
+fieldnames+=['comet-'+str(rack)+'-'+str(i) for i in np.linspace(node1,node2, node2-node1+1)]
+csvfile = open('results.csv', 'w')
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writerow(dict((fn,fn) for fn in fieldnames))
+
+for first_node in np.linspace(node1,node2, node2-node1+1):
+    writer.writerow({'#':'comet-'+str(rack)+'-'+str(first_node)})
+    for combo in array:
+        if combo[0]==first_node:
+            node_pair_index=array.index(combo)
+#            writer.writerow({'#':first_node,str(array[node_pair_index][1]):latency_times[node_pair_index]})
+
+
+print rack
 
 #if __name__ == "__main__":
 #    parser = argparse.ArgumentParser(
