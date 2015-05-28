d8af368fe20b29d6605f7d6dc16b5f4bf63274c2
Syed Sadat Nazrul <snazrul1@users.noreply.github.com>
2015-05-28 15:52:07 -0700

    Added argparser and improved documentation

--- analyze.py
+++ analyze.py
@@ -15,20 +15,11 @@ def combinations(array):
                 combo.append([i,j])
     return combo
 
-def analyze(filenames):
-    fieldnames=['#']
-    fieldnames+=[str(i) for i in np.linspace(1,18,18)]
-    csvfile = open('results.csv', 'w')
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for f in filenames:
-        r = get_results(f)
-        r["#"] = f
-        writer.writerow(r)
-    print("Results written to results.csv!")
-
 def get_results(filename):
     '''
+    Generates a CSV file of latency times
+    :param filename (string): Name of latency test output file
+    :return: CSV file with latency times
     '''
     #Extract Latency from Matrix
     file=open(filename)
@@ -68,14 +59,11 @@ def get_results(filename):
         writer.writerow(node_dict)
     print '%s created!'%(filename.replace('.out','.csv'))
 
-filename='comet-17-[12-14].out'
-get_results(filename)
-
-#if __name__ == "__main__":
-#    parser = argparse.ArgumentParser(
-#        description='''Tool for analysis of Latency Tests''')
-#    parser.add_argument(
-#        'filenames', metavar='filenames', type=str, nargs="+",
-#        help='Files to process. You may use wildcards, e.g., "python analyze.py *.out".')
-#    args = parser.parse_args()
-#    analyze(args.filenames)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='''Tool for analysis of Latency Tests''')
+    parser.add_argument(
+        'filenames', metavar='filenames', type=str, nargs="+",
+        help='Files to process. You may use wildcards, e.g., "python analyze.py *.out".')
+    args = parser.parse_args()
+    analyze(args.filenames)
