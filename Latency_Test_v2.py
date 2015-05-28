49046cc2c0afa314e575197182bbfa7df7e801f7
Syed Sadat Nazrul <snazrul1@users.noreply.github.com>
2015-05-28 16:35:30 -0700

    Added analysis to batch script

--- Latency_Test_v2.py
+++ Latency_Test_v2.py
@@ -68,6 +68,7 @@ export BINARY=/home/ssnazrul/mpi_test/osu-micro-benchmarks-4.4.1/mpi/pt2pt/osu_l
         f.write("\n")
         f.write("echo comet-%02d-%02d,comet-%02d-%02d\n"%(Rack,node1,Rack,node2))
         f.write("srun -w comet-%02d-%02d,comet-%02d-%02d -N 2 -n 2  --mpi=pmi2 $BINARY\n"%(Rack,node1,Rack,node2))
+    f.write("python analyze.py *out")
     f.close()
     subprocess.call(["sbatch","batch_script"])     #Submit Batch Script
     subprocess.call(["rm","batch_script"])         #Delete Batch Script after submission
